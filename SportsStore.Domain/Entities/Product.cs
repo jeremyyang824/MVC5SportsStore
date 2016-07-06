516f3ae542f1e7486c133fda00e26d70303101b2
jeremyyang824 <jeremyyang824@aliyun.com>
2016-07-06 12:46:23 +0800

    Chapter11 Finished

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,15 +1,25 @@
-﻿namespace SportsStore.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
 
+        [Required(ErrorMessage ="请输入商品名")]
         public string Name { get; set; }
 
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "请输入商品描述")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "请输入正确的价格")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "请指定一个分类")]
         public string Category { get; set; }
     }
 }
